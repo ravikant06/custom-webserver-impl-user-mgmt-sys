da58f7f2019b4940d1a5d96a0bde1a7bd0d795a7
Ravikant  Sharma <ravikantsharma@RavikantsLaptop.lan>
2025-08-27 16:54:09 +0530

    Update API base in config

--- js/config.js
+++ js/config.js
@@ -3,15 +3,13 @@ const CONFIG = {
   // Development environment (localhost)
   development: {
     API_BASE: 'http://localhost:8080',
-    GOOGLE_CLIENT_ID: '10217611478-r6n5miarsidp97u82kt9fcqcle9ftcb3.apps.googleusercontent.com',
-    FACEBOOK_APP_ID: 'YOUR_FACEBOOK_APP_ID'
+    GOOGLE_CLIENT_ID: '10217611478-r6n5miarsidp97u82kt9fcqcle9ftcb3.apps.googleusercontent.com'
   },
   
   // Production environment (GitHub Pages)
   production: {
-    API_BASE: 'http://localhost:8080', // Temporary: Update this when you deploy backend
-    GOOGLE_CLIENT_ID: '10217611478-r6n5miarsidp97u82kt9fcqcle9ftcb3.apps.googleusercontent.com',
-    FACEBOOK_APP_ID: 'YOUR_FACEBOOK_APP_ID'
+    API_BASE: 'https://27e9c817fd1cfac449afcddb3327341a.serveo.net', // Temporary: Update this when you deploy backend
+    GOOGLE_CLIENT_ID: '10217611478-r6n5miarsidp97u82kt9fcqcle9ftcb3.apps.googleusercontent.com'
   }
 };
 
